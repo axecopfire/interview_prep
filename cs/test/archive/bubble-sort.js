68facc20cfc33d2adb17d4b07a4a20bbcdd1fa7a
axecopfire <sbraun990@gmail.com>
2019-10-07 15:27:25 -0400

    added a todo message

--- cs/test/archive/bubble-sort.js
+++ cs/test/archive/bubble-sort.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
-var bubbleSort = require('../algos/bubble-sort');
+var bubbleSort = require('../../algos/bubble-sort');
 
+// TODOS: check to see if there are unnecesary comparisons happening
 describe('bubble sort', function () {
 	it("each item should be less than previous", function() {
 		var arr = [9,8,7,6,5,7,4,6,3,1];
