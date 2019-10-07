cdb672c7323f8d9124702955ec40b655d2458543
axecopfire <sbraun990@gmail.com>
2019-10-07 11:52:35 -0400

    working on linked list and testing

---
+++ ds/test/test.js
@@ -0,0 +1,59 @@
+var assert = require('assert');
+var { Node, LinkedList } = require('../linked-list');
+
+describe('Node', () => {
+	var node = new Node(20);
+	it('data should return 20', () => {
+		assert.equal(node.data, 20);
+	});
+
+	let bool = node.key > 0 ? true : false;
+	it('key should be greater than 0', () => {
+		assert.equal(bool, true);
+	})
+});
+
+
+describe('LinkedList', () => {
+	var ll = new LinkedList();
+	it('should have head', () => {
+		let bool = ll.hasOwnProperty('head');
+		assert.equal(bool, true);
+	})
+	it('should have tail', () => {
+		let bool = ll.hasOwnProperty('tail');
+		assert.equal(bool, true);
+	})
+	
+	// array for appending to linked list
+	var a = [0, "dfasd", 43, 234, 25, "dfsd3"];
+	describe('append', () => {
+
+		describe('init append', () => {
+			it('if empty should create head', () => {
+				ll.append(22);
+				assert.equal(ll.head.data, 22);
+			});
+	
+			it('tail equal to head', () => {
+				assert.equal(ll.tail, ll.head);
+			});
+		})
+
+		describe('Second append', () => {
+			it('should create a child of head', () => {
+				ll.append(55);
+				assert.equal(ll.head.child.data, 55);
+			});
+			it('tail should update', () => {
+				assert.equal(ll.tail, ll.head.child);
+			});
+			it("new node's prev should be head", () => {
+				assert.equal(ll.head.child.prev, ll.head);
+			}); 
+		});
+
+	})
+	
+
+})
