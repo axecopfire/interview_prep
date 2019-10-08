22ea353ab8915e3d35cd0c3ec2ca64e388f5e5b5
axecopfire <sbraun990@gmail.com>
2019-10-08 17:26:10 -0400

    codewars: spiral

---
+++ practice/spiral.js
@@ -0,0 +1,38 @@
+function createSpiral(N) {
+	// create a "map" of N arrays
+	// add N place holders in each array
+	// determine the edges
+		// loop through the first array and add i
+		// once you reach the length
+			// go to the second array and add i + 1
+			// continue until you've reached N array
+			// then loop back until you've reached the beginning of that array
+			// then go up an array and go as far as possible
+	// decrement a position from the edges
+	
+	// pivot
+	/*
+	// once I reach an edge I want to switch increment vs decrement
+	for i < vEdge; i ++; (vertical)
+		for j < hEdge; j ++ (horizontal)
+		[1,2,3] (arr[j]) until hEdge
+		pivot
+			need to traverse using i
+			j needs to start minusing
+		
+		90deg pivot (edge arr[N])
+		[,,4]
+		[,,5]
+		90deg pivot
+		[7,6,5]
+		90deg pivot
+		[8,,4]
+		90deg
+		[8,9,4]
+		done
+	
+	*/
+	
+	
+	
+	}
