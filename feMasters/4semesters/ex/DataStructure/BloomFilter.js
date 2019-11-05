0a0dfaab817aaa41a460c9822790829526618b54
axecopfire <sbraun990@gmail.com>
2019-11-05 13:57:06 -0500

    basic working bloom filter

--- feMasters/4semesters/ex/DataStructure/BloomFilter.js
+++ feMasters/4semesters/ex/DataStructure/BloomFilter.js
@@ -1,18 +1,59 @@
+// Currently doesn't handle collisions
+// Also only sets true or false, does not save any other data
+
 function BloomFilter (len = 500) {
+  this.len = len;
   this.table = new Array(len);
 }
 
 BloomFilter.prototype.Hash = function (str, int) {
   switch(int) {
     case 0: 
-      return Math.floor(str.CharCodeAt() % len); 
+      return Math.floor(str.charCodeAt() % this.len); 
     case 1:
-      return Math.floor(str[0].CharCodeAt() % len);
+      return Math.floor(str[0].charCodeAt() % this.len);
     case 2:
-      return Math.floor((str[0].CharCodeAt() * 5) % len);
+      return Math.floor((str[0].charCodeAt() * 5) % this.len);
+    default:
+      return false;
   }
 } 
 
+// Add true to each hashed position
 BloomFilter.prototype.add = function (str) {
+   let counter = 0;
+   let hashed = this.Hash(str, counter);
+ 
+   while(hashed) {
+     if(!this.table[hashed]) {
+       this.table[hashed] = true;
+     }
+     counter++;
+     hashed = this.Hash(str, counter);
+   }
 
+   return true;
+}
+
+// Check all hashed positions returns false if at least 1 position is empty.
+BloomFilter.prototype.check = function (str) {
+  let counter = 0;
+  let hashed = this.Hash(str, counter);
+
+  while(hashed) {
+    if(!this.table[hashed]) {
+      return false;
+    }
+    counter++;
+    hashed = this.Hash(str, counter);
+  }
+  return true;
+}
+
+BloomFilter.prototype.read = function () {
+  let res = [];
+  for(let i = 0; i < this.table.length; i ++) {
+    if(this.table[i]) res.push(i);
+  }
+  return JSON.stringify(res);
 }
